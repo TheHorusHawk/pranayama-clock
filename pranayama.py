29d137078deb7dae834399752db6f464cb408442
The Horus Hawk <brunomadteles@gmail.com>
2014-08-23 12:18:53 +0100

    Accepts input from user regarding breathing times. Added bell hit for changes to apnea.

--- pranayama.py
+++ pranayama.py
@@ -1,10 +1,28 @@
 import math
 from threading import Timer
 import pyglet
+from Tkinter import *
+
 
 inhale = 20
 exhale = 10
 apneia = 0
+
+def get_input():
+	global inhale, exhale, apneia
+	inhale = raw_input("How many seconds on the inhale?")
+	exhale = raw_input("How many seconds on the exhale?")
+	apneia = raw_input("How many seconds for apnea?")
+	if (inhale.isdigit() and exhale.isdigit() and apneia.isdigit()):
+		inhale = int(inhale)
+		exhale = int(exhale)
+		apneia = int(apneia)
+	else:
+		print "Input incorrect. Please try again"
+		get_input()
+
+
+get_input()
 totaltime = inhale + exhale + 2 * apneia
 sound = pyglet.resource.media('Bell.wav', streaming=False)
 
@@ -14,6 +32,7 @@ current = 0
 state = "" 
 total = 0 
 
+#formats time
 def formater (seconds):
 	string = ""
 	second = math.floor(seconds%60)
@@ -21,9 +40,11 @@ def formater (seconds):
 	string = "Time elapsed " + str(minute) + ":" + str(second) 
 	return string
 
+# plays the sound 
 def hit_bell ():
 	sound.play()
 
+#augments time and updates state
 def augment (dt):
 	print dt	
 	global current, state, total, label
@@ -36,12 +57,16 @@ def augment (dt):
 			hit_bell()
 		state = "Inhale" 
 	elif (current > inhale and current <= inhale + apneia):
+		if (current == inhale + 1) :
+			hit_bell()
 		state = "Apnea"
 	elif (current > inhale + apneia and current <= inhale + apneia + exhale):
 		if (current == inhale + apneia + 1):
 			hit_bell()
 		state = "Exhale" 
 	else:
+		if (current == inhale + apneia + exhale + 1) :
+			hit_bell()
 		state = "Apnea"
 	print str(current) + " " + str(state) + "                               " + formater(total),
 	draw_label()
@@ -57,6 +82,7 @@ def draw_label():
 	window.clear()
 	label.draw()
 
+#calls augment every second
 pyglet.clock.schedule_interval(augment, 1)
 	
 pyglet.app.run()
